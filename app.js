d03cd23bb09a017ba64a23e23a56e40c3c7e4abf
visserjackson-code <jcv74@proton.me>
2026-03-28 14:44:24 -0400

    pokeapi somewhat integrated

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //fetch json on page load and store inside variable
 import encounters from './encounters.json' with { type: 'json' };
 
+
+
 const starters = encounters.starters;
 const r101 = encounters.r101;
 
@@ -17,6 +19,8 @@ const r101Dropdown = document.getElementById('101-choose');
 const r103Dropdown = document.getElementById('103-choose');
 const r102Dropdown = document.getElementById('102-choose');
 
+
+
 const calculatePercentageOdds = (rate) => {
     if (isNaN(rate)) return rate;
 
@@ -25,6 +29,21 @@ const calculatePercentageOdds = (rate) => {
     }
 
 }
+
+const getSprite = async (mon) => {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${mon.toLowerCase()}`);
+    const data = await response.json();
+    return data.sprites.front_default;
+}
+
+const mudkipHeader = document.getElementById('starter-1');
+const treeckoHeader = document.getElementById('starter-2');
+const torchicHeader = document.getElementById('starter-3');
+mudkipHeader.src = await getSprite('mudkip');
+treeckoHeader.src = await getSprite('treecko');
+torchicHeader.src = await getSprite('torchic');
+
+
 const renderEncounterOptions = (areaData, areaName, targetDropdown) => {
     const template = document.getElementById("encounter-option-template");
     targetDropdown.innerHTML = '';
@@ -50,3 +69,5 @@ renderEncounterOptions(encounters['starters'], 'Starters', starterDropdown);
 renderEncounterOptions(encounters['r101'], 'Route 101', r101Dropdown);
 renderEncounterOptions(encounters['r103'], 'Route 103', r103Dropdown);
 renderEncounterOptions(encounters['r102'], 'Route 102', r102Dropdown);
+
+
